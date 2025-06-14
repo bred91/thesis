d34aae2fbe25f2b198e735cb156acffbf549eb90
Raffaele Pane <58046025+bred91@users.noreply.github.com>
2025-06-14 16:08:25 +0200

    feat: sqlite schema

---
+++ db_sqllite/sqllite_init.sql
@@ -0,0 +1,36 @@
+-- SQLite database schema for the project
+
+-- logs table to store log entries
+CREATE TABLE IF NOT EXISTS logs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TEXT,
+    level TEXT,
+    message TEXT
+);
+
+-- commits table to store commit information
+CREATE TABLE IF NOT EXISTS commits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    commit_hash TEXT UNIQUE,
+    author TEXT,
+    date TEXT,
+    message TEXT,
+    files TEXT,
+    diffs TEXT
+);
+
+-- summaries table to store summaries related to commits for a specific experiment
+CREATE TABLE IF NOT EXISTS summaries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    commit_id INTEGER,
+    experiment_name TEXT,
+    date TEXT,
+    llama_category TEXT,
+    llama_summary TEXT,
+    llama_summary_retrieved_docs TEXT,
+    llama_tech_summary TEXT,
+    llama_tech_summary_retrieved_docs TEXT,
+    
+    FOREIGN KEY (commit_id) REFERENCES commits(id)
+)
+
