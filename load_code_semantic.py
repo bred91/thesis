416b830ecfb6abaaedb3c736f4627540d127d9d6
Raffaele Pane <58046025+bred91@users.noreply.github.com>
2025-08-22 12:51:22 +0200

    feat: added custom metadata to semantic code collection

--- load_code_semantic.py
+++ load_code_semantic.py
@@ -1,9 +1,12 @@
+import os
+from pathlib import Path
+
 from langchain_chroma import Chroma
 from langchain_community.document_loaders.git import GitLoader
 from langchain_ollama import OllamaEmbeddings
 
-from utils.config import CHROMA_PERSIST_DIR, CHROMA_METADATA, EMBEDDING_MODEL, MUJS_LOCAL_PATH, MUJS_BRANCH, \
-    SEMANTIC_CODE_COLLECTION
+from utils.config import CHROMA_PERSIST_DIR, CHROMA_METADATA, MUJS_LOCAL_PATH, MUJS_BRANCH, \
+    SEMANTIC_CODE_COLLECTION, EMBEDDING_MODEL
 
 embeddings = OllamaEmbeddings(model=EMBEDDING_MODEL)
 
@@ -13,10 +16,25 @@ def build_mujs_code_index():
         repo_path=MUJS_LOCAL_PATH,
         clone_url=None,
         branch=MUJS_BRANCH,
-        file_filter=lambda fp: fp.endswith((".c", ".h", "*.md")) or fp == "Makefile",
+        file_filter=lambda fp: fp.endswith((".c", ".h", ".md")) or fp == "Makefile",
     )
     docs = loader.load()
 
+    for d in docs:
+        src = d.metadata.get("source") or d.metadata.get("file_path") or ""
+        full_name = os.path.basename(src)
+        stem = Path(full_name).stem.lower()
+        ext = Path(full_name).suffix.lower()
+        # update metadata
+        d.metadata.update({
+            "path": src,                # original path if available
+            "file_name": full_name,     # e.g., "opnames.h"
+            "stem": stem,               # e.g., "opnames"
+            "ext": ext,                 # e.g., ".h"
+        })
+        d.page_content = f"FILE NAME: {src}\n---\nCONTENT:{d.page_content}"
+
+    # store docs in chroma
     store = Chroma(
         collection_name=SEMANTIC_CODE_COLLECTION,
         embedding_function=embeddings,
@@ -27,6 +45,7 @@ def build_mujs_code_index():
     store.add_documents(docs)
     return store
 
+#todo: remove
 def main():
     build_mujs_code_index()
 
